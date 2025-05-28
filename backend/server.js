c6a9ce1bd22ee259d6772207a1a2b3ffceac6a04
alepiacentini <153805072+alepiacentini@users.noreply.github.com>
2025-05-28 10:34:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-  origin: 'https://bookstoreonline1.vercel.app/',
+  origin: 'https://bookstoreonline1.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
